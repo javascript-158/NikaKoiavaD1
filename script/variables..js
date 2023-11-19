e5801a63fdb4a4bc5f43c95876ee489401d7a6bd
Nikoloz Koiava <koiavanika0@gmail.com>
2023-11-20 02:10:41 +0400

    Davaleba1

---
+++ script/variables..js
@@ -0,0 +1,12 @@
+let fullname = prompt("What is you fullname?")
+let year = prompt("when were you born?");
+let position  = prompt("where do you live?");
+let age = 2023 - year;
+alert(
+  "Hello, " +
+    fullname +
+    ", Your age is " +
+    age +
+    ", Your location is " +
+    position
+);
